41f4e6e327add3ff70aa964e98f963a99ff3b7c0
bigsawka@gmail.com <Qbmsu21RDcfouu>
2017-09-28 14:05:59 +0300

    ruudustik

--- MainForm29.08.17/PA_29_09_2017.Designer.cs
+++ MainForm29.08.17/PA_29_09_2017.Designer.cs
@@ -39,8 +39,9 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.PA_JoonLaius = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PA_Ruudustik = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.PA_pictureBox)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -58,7 +59,7 @@
             // PA_Joon
             // 
             this.PA_Joon.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.PA_Joon.Location = new System.Drawing.Point(19, 114);
+            this.PA_Joon.Location = new System.Drawing.Point(44, 114);
             this.PA_Joon.Name = "PA_Joon";
             this.PA_Joon.Size = new System.Drawing.Size(121, 45);
             this.PA_Joon.TabIndex = 1;
@@ -78,7 +79,7 @@
             // PA_X2
             // 
             this.PA_X2.AutoSize = true;
-            this.PA_X2.Location = new System.Drawing.Point(82, 22);
+            this.PA_X2.Location = new System.Drawing.Point(103, 21);
             this.PA_X2.Name = "PA_X2";
             this.PA_X2.Size = new System.Drawing.Size(38, 18);
             this.PA_X2.TabIndex = 3;
@@ -88,25 +89,25 @@
             // 
             this.PA_X1koordinaat.Location = new System.Drawing.Point(44, 18);
             this.PA_X1koordinaat.Name = "PA_X1koordinaat";
-            this.PA_X1koordinaat.Size = new System.Drawing.Size(24, 26);
+            this.PA_X1koordinaat.Size = new System.Drawing.Size(53, 26);
             this.PA_X1koordinaat.TabIndex = 4;
             this.PA_X1koordinaat.Text = "0";
             this.PA_X1koordinaat.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // PA_X2koordinaat
             // 
-            this.PA_X2koordinaat.Location = new System.Drawing.Point(126, 18);
+            this.PA_X2koordinaat.Location = new System.Drawing.Point(147, 17);
             this.PA_X2koordinaat.Name = "PA_X2koordinaat";
-            this.PA_X2koordinaat.Size = new System.Drawing.Size(24, 26);
+            this.PA_X2koordinaat.Size = new System.Drawing.Size(50, 26);
             this.PA_X2koordinaat.TabIndex = 5;
             this.PA_X2koordinaat.Text = "0";
             this.PA_X2koordinaat.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // PA_Y2koordinaat
             // 
-            this.PA_Y2koordinaat.Location = new System.Drawing.Point(126, 48);
+            this.PA_Y2koordinaat.Location = new System.Drawing.Point(147, 47);
             this.PA_Y2koordinaat.Name = "PA_Y2koordinaat";
-            this.PA_Y2koordinaat.Size = new System.Drawing.Size(24, 26);
+            this.PA_Y2koordinaat.Size = new System.Drawing.Size(50, 26);
             this.PA_Y2koordinaat.TabIndex = 9;
             this.PA_Y2koordinaat.Text = "0";
             this.PA_Y2koordinaat.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
@@ -115,7 +116,7 @@
             // 
             this.PA_Y1koordinaat.Location = new System.Drawing.Point(44, 48);
             this.PA_Y1koordinaat.Name = "PA_Y1koordinaat";
-            this.PA_Y1koordinaat.Size = new System.Drawing.Size(24, 26);
+            this.PA_Y1koordinaat.Size = new System.Drawing.Size(53, 26);
             this.PA_Y1koordinaat.TabIndex = 8;
             this.PA_Y1koordinaat.Text = "0";
             this.PA_Y1koordinaat.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
@@ -123,7 +124,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(82, 51);
+            this.label2.Location = new System.Drawing.Point(103, 50);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(38, 18);
             this.label2.TabIndex = 7;
@@ -153,10 +154,19 @@
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(161, 171);
+            this.groupBox1.Size = new System.Drawing.Size(205, 171);
             this.groupBox1.TabIndex = 10;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Joon";
+            // 
+            // PA_JoonLaius
+            // 
+            this.PA_JoonLaius.Location = new System.Drawing.Point(100, 82);
+            this.PA_JoonLaius.Name = "PA_JoonLaius";
+            this.PA_JoonLaius.Size = new System.Drawing.Size(40, 26);
+            this.PA_JoonLaius.TabIndex = 11;
+            this.PA_JoonLaius.Text = "0";
+            this.PA_JoonLaius.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // label1
             // 
@@ -167,14 +177,16 @@
             this.label1.TabIndex = 10;
             this.label1.Text = "Laius =";
             // 
-            // PA_JoonLaius
+            // PA_Ruudustik
             // 
-            this.PA_JoonLaius.Location = new System.Drawing.Point(100, 82);
-            this.PA_JoonLaius.Name = "PA_JoonLaius";
-            this.PA_JoonLaius.Size = new System.Drawing.Size(24, 26);
-            this.PA_JoonLaius.TabIndex = 11;
-            this.PA_JoonLaius.Text = "0";
-            this.PA_JoonLaius.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.PA_Ruudustik.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.PA_Ruudustik.Location = new System.Drawing.Point(56, 205);
+            this.PA_Ruudustik.Name = "PA_Ruudustik";
+            this.PA_Ruudustik.Size = new System.Drawing.Size(121, 45);
+            this.PA_Ruudustik.TabIndex = 12;
+            this.PA_Ruudustik.Text = "Ruudustik";
+            this.PA_Ruudustik.UseVisualStyleBackColor = false;
+            this.PA_Ruudustik.Click += new System.EventHandler(this.PA_Ruudustik_Click);
             // 
             // PA_29_09_2017
             // 
@@ -183,6 +195,7 @@
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(781, 642);
+            this.Controls.Add(this.PA_Ruudustik);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.PA_pictureBox);
             this.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
@@ -211,5 +224,6 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TextBox PA_JoonLaius;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button PA_Ruudustik;
     }
 }
--- MainForm29.08.17/PA_29_09_2017.cs
+++ MainForm29.08.17/PA_29_09_2017.cs
@@ -12,29 +12,77 @@ namespace MainForm29._08._17
 {
     public partial class PA_29_09_2017 : Form
     {
+        float nx = 8, ny = 10;
+        float x1, x2, y1, y2, joonLaius;
+        Pen Pliiats = new Pen(Color.Black, 1);// (цвет и тощина)
+        SolidBrush Pintset = new SolidBrush(Color.Green);
+        Graphics g;
+
+        private void PA_Ruudustik_Click(object sender, EventArgs e)
+        {
+            for(float i = 1; i <= nx; i++)
+            {
+                
+                float t = GBlaius / nx;
+                x1 = t*i;
+                y1 = 0;
+                x2 = t*i;
+                y2 = GBkorgus;
+ 
+                g.DrawLine(Pliiats, x1, y1, x2, y2);
+            }
+            for (float i = 1; i <= ny; i++)
+            {
+                
+                float t = GBkorgus / ny;
+                x1 = 0;
+                y1 = t * i;
+                x2 = GBlaius;
+                y2 = t * i;
+
+                g.DrawLine(Pliiats, x1, y1, x2, y2);
+            }
+
+        }
 
         Random r = new Random();
-        int laius, korgus;
+        int GBlaius, GBkorgus;
         public PA_29_09_2017()
         {
             InitializeComponent();
+            GBlaius = PA_pictureBox.Width; 
+            GBkorgus = PA_pictureBox.Height;
+            g = Graphics.FromHwnd(PA_pictureBox.Handle);
         }
 
         private void Juhuarvud()
         {
-            int x1 = r.Next(laius);
-            int x2 = r.Next(laius);
-            int y1 = r.Next(korgus);
-            int y2 = r.Next(korgus);
+             x1 = r.Next(GBlaius);
+             x2 = r.Next( GBlaius);
+             y1 = r.Next(GBkorgus);
+             y2 = r.Next( GBkorgus);
+             joonLaius = r.Next(1, 10);
 
             PA_X1koordinaat.Text = x1.ToString();
-
+            PA_X2koordinaat.Text = x2.ToString();
+            PA_Y1koordinaat.Text = y1.ToString();
+            PA_Y2koordinaat.Text = y2.ToString();
+            PA_JoonLaius.Text = joonLaius.ToString();
+            Pliiats.Width = r.Next(5, 15);
+            int varv = r.Next(54);
+            int punane = r.Next(254);
+            int roheline = r.Next(254);
+            int sinine = r.Next(254);
+            Pliiats.Color = Color.FromArgb(punane, roheline, sinine);
         }
 
 
         private void PA_Joon_Click(object sender, EventArgs e)
         {
             Juhuarvud();
+            g.DrawLine(Pliiats, x1, y1, x2, y2);
+
+
         }
     }
 }
